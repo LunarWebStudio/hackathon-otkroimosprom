6923566edfaac9bb787ceea5368b53e75042a654
Denis Smirnov <w1png@yandex.ru>
2025-10-17 15:58:13 +0300

    add role check middleware

--- apps/server/src/orpc/orpc.ts
+++ apps/server/src/orpc/orpc.ts
@@ -1,9 +1,8 @@
+import type { UserRole } from "@lunarweb/shared/types";
 import { ORPCError, os } from "@orpc/server";
 import type { ORPCContext } from "./context";
 
 export const o = os.$context<ORPCContext>();
-
-export const publicProcedure = o;
 
 const requireAuth = o.middleware(async ({ context, next }) => {
 	if (!context.session?.user) {
@@ -16,4 +15,30 @@ const requireAuth = o.middleware(async ({ context, next }) => {
 	});
 });
 
+const errorLogger = o.middleware(async ({ context, next }) => {
+	try {
+		return await next({ context });
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+});
+
+export const publicProcedure = o.use(errorLogger);
+
 export const protectedProcedure = publicProcedure.use(requireAuth);
+
+export const roleProcedure = (roles: UserRole[]) => {
+	return protectedProcedure.use(async ({ context, next, path }) => {
+		if (!roles.includes(context.session.user.role as UserRole)) {
+			throw new ORPCError("FORBIDDEN", {
+				message: `User has role: ${context.session.user.role} but ${roles.join(", ")} are required for ${path.join(".")}`,
+			});
+		}
+		return next({
+			context: {
+				session: context.session,
+			},
+		});
+	});
+};
