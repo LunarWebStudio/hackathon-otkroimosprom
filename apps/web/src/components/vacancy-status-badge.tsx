87a1632fdb9198fab33c61d44ed6eb860036bdcf
GeoLise <jira.liseenko@yandex.ru>
2025-10-19 05:55:22 +0300

    change vacancy status color

--- apps/web/src/components/vacancy-status-badge.tsx
+++ apps/web/src/components/vacancy-status-badge.tsx
@@ -10,7 +10,7 @@ export const vacancyStatusBadgeVariants = cva(
 				REJECTED: "bg-red-100 text-red-900",
 				APPROVED: "bg-green-100 text-green-900",
 				PENDING: "bg-yellow-100 text-yellow-900",
-				COMPLETED: "bg-violet-900 text-violet-100",
+				COMPLETED: "bg-violet-100 text-violet-900",
 			},
 		},
 	},
